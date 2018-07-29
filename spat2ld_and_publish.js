d32ba4d7c0776f94c5bc28f89918b60ec94559ef
brechtvdv <br5cht@hotmail.com>
2018-07-29 17:09:12 +0200

    One JSON-LD object for all signalgroups

--- spat2ld_and_publish.js
+++ spat2ld_and_publish.js
@@ -3,16 +3,15 @@ const jsonld = require('jsonld');
 const url = require('url');
 const http2 = require('http2');
 const fs = require('fs');
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://146.253.51.199:30201')
- 
-client.on('connect', function () {
-  client.subscribe('K648/spat/json', {'qos': 2})
-})
+const md5 = require('md5');
 
 const MILLIS_THIS_YEAR = moment([moment().year()]).valueOf();
 
 let signalGroups = [];
+let response = []; // add all json-ld objects in this
+let responseTemp = []; // replaces response when new message has been processed completely
+let minMaxAge = 100;
+let minMaxAgeTemp = 100;
 
 // Analytic purposes
 var counterNoChanges = 0;
@@ -49,14 +48,18 @@ const signalGroupContext = {
 process.stdin.on('data', function (data) {
   try {
     if (data != null) {
-      const spat =  JSON.parse(data);
-      // console.log(spat)
+      const spat = JSON.parse(data.toString().split('\n')[0]); // sometimes multiple SPAT messages in one data object
     	const moy = spat.spat.intersections[0].moy; 
     	const timestamp = spat.spat.intersections[0].timeStamp;
       const graphGeneratedAtString = calcTime(moy, timestamp).utc().add(1, 'hour').format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
     	const graphGeneratedAtDate = calcTime(moy, timestamp).utc().add(1, 'hour');
 
       const states = spat.spat.intersections[0].states;
+
+      // reset response for every spat message
+      responseTemp = [];
+      minMaxAgeTemp = 100;
+
       // console.log("------------------------")
       for (let i=0; i<states.length; i++) {
       	// Get data
@@ -91,64 +94,22 @@ process.stdin.on('data', function (data) {
       		]
       	}
 
-        console.log(JSON.stringify(doc));
+        // For caching
+        const maxAge = (minEndTimeDate.valueOf() - graphGeneratedAtDate.valueOf())/1000; // seconds
+        if (maxAge < minMaxAgeTemp) minMaxAgeTemp = maxAge;
 
-        // Push to listeners SSE
+        responseTemp.push(doc);
+
+        // Push everything to listeners SSE
         sendUpdateToListeners(doc);
-
-        // Check if version exists
-        if(!signalGroups[signalGroupUri]){
-          signalGroups[signalGroupUri] = {};
-          signalGroups[signalGroupUri].jsonld = doc;
-          signalGroups[signalGroupUri].minEndTimeDate = minEndTimeDate;
-          signalGroups[signalGroupUri].maxEndTimeDate = maxEndTimeDate;
-          signalGroups[signalGroupUri].graphGeneratedAtDate = graphGeneratedAtDate;
-        } else {
-          let currentSignalGroup = signalGroups[signalGroupUri];
-          // Check if this changes previous version
-          if (eventStateName != currentSignalGroup.jsonld['@graph'][0].eventState['rdfs:label']
-            || minEndTimeDate.valueOf() > currentSignalGroup.minEndTimeDate.valueOf()+2
-            || maxEndTimeDate.valueOf() < currentSignalGroup.maxEndTimeDate.valueOf()-2) {
-            // console.log("Previousmin:" + currentSignalGroup.minEndTimeDate.format() + "--- Now: " + minEndTimeDate.format());
-            // console.log("Previousmin:" + currentSignalGroup.minEndTimeDate + "--- Now: " + minEndTimeDate);
-            // console.log("Previousmax:" + currentSignalGroup.maxEndTimeDate.format() + "--- Now: " + maxEndTimeDate.format());
-            // console.log("Previousmax:" + currentSignalGroup.maxEndTimeDate + "--- Now: " + maxEndTimeDate);
-            // console.log("Previous state:" + currentSignalGroup.jsonld['@graph'][0].eventState['rdfs:label'] + "--- Now: " + eventStateName)
-            counter++;
-            // TODO Archive previous version
-            // if (signalGroupUri === 'http://example.org/signalgroup/1') {
-            //     console.log(graphGeneratedAtString);
-            //     console.log((currentSignalGroup.minEndTimeDate.valueOf() - currentSignalGroup.graphGeneratedAtDate.valueOf())/1000)
-            //     console.log(currentSignalGroup.jsonld['@graph'][0].eventState['rdfs:label'] + ' - ' + currentSignalGroup.jsonld['@graph'][0].minEndTime)
-            //     console.log('-----------')
-            // }
-            // Update current version
-            let newSignalGroup = {};
-            newSignalGroup.jsonld = doc;
-            newSignalGroup.minEndTimeDate = minEndTimeDate;
-            newSignalGroup.maxEndTimeDate = maxEndTimeDate;
-            newSignalGroup.graphGeneratedAtDate = graphGeneratedAtDate;
-            newSignalGroup.maxAge = (minEndTimeDate.valueOf() - graphGeneratedAtDate.valueOf())/1000; // seconds
-            signalGroups[signalGroupUri] = newSignalGroup;
-          } else if (eventStateName != currentSignalGroup.jsonld['@graph'][0].eventState['rdfs:label']
-            || minEndTimeDate.valueOf() < currentSignalGroup.minEndTimeDate.valueOf()
-            || maxEndTimeDate.valueOf() > currentSignalGroup.maxEndTimeDate.valueOf()) {
-            // should only happens when emergencies happen
-            // should be pushed to the client
-          } else {
-          }
-        }
       }
-      // if (counter > 0) counterChanges++; else counterNoChanges++;
-      // counter = 0;
-      // console.log(counterChanges + " (changes)/" + counterNoChanges + " no changes");
-      // console.log((minEndTimeSpat.valueOf() - graphGeneratedAtDate.valueOf())/1000)
-      // minEndTimeSpat = null;
-      // counter = 0;
+      response = responseTemp;
+      minMaxAge = minMaxAgeTemp;
+      //console.log(minMaxAge);
     }
   } catch(error) {
     console.error(data.toString());
-      console.error(error);
+    console.error(error);
   }
 });
 
@@ -170,7 +131,8 @@ function calcTime(moy, timestamp) {
 // SERVER
 const server = http2.createSecureServer({
   key: fs.readFileSync('./keys/localhost-privkey.pem'),
-  cert: fs.readFileSync('./keys/localhost-cert.pem')
+  cert: fs.readFileSync('./keys/localhost-cert.pem'),
+  allowHTTP1: true
 }, onRequest);
 server.on('error', (err) => console.error(err));
 process.on('uncaughtException', function (err) {
@@ -191,22 +153,13 @@ function onRequest (req, res) {
         listeners.splice( listeners.indexOf(res), 1 )
       });
     } else {
-      const timeRequest = moment(); // when the request happens
+      // Regular request
       res.setHeader('Access-Control-Allow-Origin', '*');
-      const uri = url.parse(req.url, true).query.uri;
-      const signalGroup = signalGroups[uri];
-
-      if (signalGroup) {
-        // Calc max-age, relates to time of request
-        res.setHeader('Content-Type', 'application/ld+json');
-
-        const maxAge = (signalGroup.minEndTimeDate.valueOf() - signalGroup.graphGeneratedAtDate.valueOf())/1000
-        res.setHeader('Cache-Control', 'public, max-age=' + Math.floor(maxAge));
-        res.end(JSON.stringify(signalGroup.jsonld));
-      } else {
-        res.writeHead(404, {});
-        res.end('Not found');
-      }
+      res.setHeader('Content-Type', 'application/ld+json');
+      let etag = 'W/"' + md5(response) + '"';
+      res.setHeader('ETag', etag);
+      res.setHeader('Cache-Control', 'public, max-age=' + Math.floor(minMaxAge));
+      res.end(JSON.stringify(response));
     }
   } catch (e) {
     // console.error(e);
